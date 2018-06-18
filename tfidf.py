6423737ef8401950b7fe948ddec9bb9dac3951cc
Kamilla Mansurova <mansurov@fzi.de>
2018-06-18 21:56:25 +0200

    tfiidf

---
+++ tfidf.py
@@ -0,0 +1,52 @@
+from sklearn.feature_extraction.text import TfidfTransformer
+from fiesta.BagOfWords import BoW
+
+
+def tf (file = None, document = None, indexOfDocument = None, scaled = False): 
+    
+    termFrequency = BoW (file = file, document=document, indexOfDocument=indexOfDocument)    
+    if scaled == True:
+        termFrequency = scale (termFrequency)
+         
+    return termFrequency
+
+def idf (file = None, document = None, smooth = True):
+    termFrequency = tf (file = file, document = document)
+    if smooth == False:
+        transformer = TfidfTransformer(smooth_idf=False)
+    else:
+        transformer = TfidfTransformer()
+
+    transformer.fit_transform(termFrequency).toarray()
+   
+    return transformer.idf_
+
+    
+def tfidf (file = None, document = None, smooth = True, indexOfDocument = None):
+    termFrequency = tf (file = file, document = document)
+    if smooth == False:
+        transformer = TfidfTransformer(smooth_idf=False)
+    
+    else:
+        transformer = TfidfTransformer()
+    tfidf = transformer.fit_transform(termFrequency).toarray()
+    
+    if indexOfDocument != None:
+   
+        return tfidf[indexOfDocument]
+   
+    else: 
+        return tfidf
+    
+def scale ( termFrequency  ) :
+
+    if len(termFrequency) == 1:
+        scaledTermFrequency = termFrequency / max (termFrequency)
+        return scaledTermFrequency
+    else:
+        scaledTermFrequency = []
+        for term in termFrequency:
+            scaledTerm = term / max (term)
+            scaledTermFrequency.append(scaledTerm)
+        return scaledTermFrequency
+    
