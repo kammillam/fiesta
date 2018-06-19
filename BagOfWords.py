6da4cc59f9bada17d6a5966bc0ffb5352ac3c314
Kamilla Mansurova <mansurov@fzi.de>
2018-06-19 10:03:04 +0200

    document and file are allowed as parameters

--- BagOfWords.py
+++ BagOfWords.py
@@ -1,17 +1,17 @@
 from sklearn.feature_extraction.text import CountVectorizer
 
-def BoW (document = None, file = None, indexOfDocument = None):
+def BoW (document = None, file = None, indexOfDocument = None, vocabulary = False):
    
     vectorizer = CountVectorizer()
+    corpus = []
     if file != None :    
-        corpus = []
         file = open(file, "r")
         for line in file:
             corpus.append(line)
         file.close()  
     
-    elif document != None:
-        corpus = document
+    if document != None:
+        corpus += document
     
     termDocumentMatrix = vectorizer.fit_transform(corpus).toarray()
     
