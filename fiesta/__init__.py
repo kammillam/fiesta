2ceb1d9209b156eb5570263f97c4555dd6845092
Kamilla Mansurova <mansurov@fzi.de>
2018-12-04 01:20:19 +0100

    init changed

--- fiesta/__init__.py
+++ fiesta/__init__.py
@@ -1,4 +1,5 @@
-from fiesta.preparation import extract_features, select_features, extract_relevant_features
+from fiesta.automatic.preparation import extract_features, select_features, extract_relevant_features
 from fiesta.preprocessing import tokenization, n_grams_tokenization, stemming, stop_words, lemmatizer, stemming
 from fiesta.feature_extraction import bag_of_words, words_counting, term_frequency, inverse_document_frequency, tfidf
 from fiesta.feature_selection import term_frequency_selection, tfidf_selection, information_gain, chi_square, latent_semantic_analysis
+from fiesta.transformers import document_transformer
